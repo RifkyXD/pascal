52846a72bf43808d55b97374ec61149cb33d589d
RifkyXD <muhammad.rifky.rahma@gmail.com>
2022-04-27 04:49:46 +0800

    pascal

--- Login/login v2.1/tooladmin.pas
+++ Login/login v2.1/tooladmin.pas
@@ -1,102 +1,129 @@
-program haya;
-uses crt;
+program toolfixadmin; 
+
+uses
+  crt;
+
+
 var
-pilih:integer;
-user,pass,username,password:string;
-f,t:text;
+  pilih: integer;
+  user, pass, username, password: string;
+  f, t,l: text;
 
 procedure get;
-begin
-assign(t,'/admin/admin');
-reset(t);
-readln(t,username);
-readln(t,password);
-end;
 
+begin
+end; 
 procedure cek;
+
 begin
-get;
-
-
-end;
-
+  assign(t, '/admin/admin');
+  reset(t);
+  readln(t, username);
+  readln(t, password);
+  close(t);
+end; 
 procedure liatsemuauser;
-begin
-clrscr;
-  assign(f,'/admin/listakun');
-  reset(f);
-  while not eoln(f) do
-begin
-  readln(f,pass);
-  writeln('•',pass)
-  end;
-  readln;
-end;
 
-procedure resetuser;
 begin
-  assign(t,'/admin/akunlist');
-  assign(f,'/admin/listakun');
+  clrscr;
+  assign(f, '/admin/listakun');
+  reset(f);
+  while not eoln(f) do 
+    begin
+      readln(f, pass);
+      writeln('•', pass) 
+    end;
+  close(f);
+  readln;
+end; 
+procedure resetuser;
+
+begin
+  assign(t, '/admin/akunlist');
+  assign(f, '/admin/listakun');
   rewrite(f);
   rewrite(t);
-  writeln(t,'');
-  writeln(f,'');
+  writeln(t, '');
+  writeln(f, '');
+  close(f);
+  close(t);
+  clrscr;
   writeln('selesai');
   delay(1000);
-end;
-
+end; 
 procedure hapussemuaakun;
-begin
-assign(t,'/admin/listakun');
-reset(t);
-while not eoln(t) do
-begin
-readln(t,user);
-assign(f,'/user/user');
-reset(f);
-erase(f);
-end;
-writeln('selesai');
-delay(1000);
-end;
-
 
 begin
-repeat
-clrscr;
-writeln('login admin');
-write('user : ');
-readln(user);
-write('pass : ');
-readln(pass);
-cek;
-if (user<>username) then
-writeln('username/password salah');
-delay(1000);
+  assign(t, '/admin/listakun');
+  reset(t);
+  while not eoln(t) do 
+    begin
+      readln(t, user);
+      assign(l, '/akun/' + user);
+      erase(l);
+     
+    end;
+    close(t);
+  clrscr;
+  writeln('selesai');
+  delay(1000);
+end; 
+procedure perbaikicookie;
 
-until(user=username) and (pass=password);
-if (user=username) and (pass=password) then
 begin
-repeat
-clrscr;
-writeln('1. hapus data semua akun');
-writeln('2. hapus semua user');
-writeln('3. liat semua user');
-writeln('0. exit');
-write('pilih : ');
-readln(pilih);
+  assign(t, '/admin/cookie.txt');
+  rewrite(t);
+  writeln(t, '0');
+  writeln(t, 'b');
+  writeln(t, 'c');
+  close(t);
+end; 
+procedure reset;
 
-case pilih of
-1:hapussemuaakun();
-2:resetuser();
-3:liatsemuauser();
-0:exit;
-else
 begin
-writeln('pilihan tidak ada');
-delay(1000);
-end;
-end;
-until pilih=0
-end;
-end.
+  hapussemuaakun;
+  resetuser;
+  perbaikicookie;
+  clrscr;
+  writeln('Selesai');
+  delay(1000);
+end; 
+
+begin
+  repeat 
+    clrscr;
+    writeln('login admin');
+    write('user : ');
+    readln(user);
+    write('pass : ');
+    readln(pass);
+    cek;
+    if (user <> username) then
+      writeln('username/password salah') ;
+    delay(1000);
+  until (user = username) and (pass = password);
+  if (user = username) and (pass = password) then
+    begin
+      repeat 
+        clrscr;
+        writeln('1. hapus data semua akun');
+        writeln('2. hapus semua user');
+        writeln('3. liat semua user');
+        writeln('4. reset ');
+        writeln('0. exit');
+        write('pilih : ');
+        readln(pilih);
+        case pilih of 
+          1: hapussemuaakun();
+          2: resetuser();
+          3: liatsemuauser();
+          4: reset();
+          0: exit;
+          else 
+          begin
+            writeln('pilihan tidak ada');
+            delay(1000);
+          end;
+        end;
+      until pilih = 0 end;
+    end .
